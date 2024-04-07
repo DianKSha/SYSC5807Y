dd8cd023ea90e108bd6f3d5ba92cf578a5ca40dc
well <dksha5328@gmail.com>
2024-04-06 21:50:52 -0400

    usageexample and simpleserial

--- simpleserial-ascon80pq/simpleserial-ascon.c
+++ simpleserial-ascon80pq/simpleserial-ascon.c
@@ -34,7 +34,7 @@
 #define NONCE_BYTES 16
 #define KEY_BYTES 20
 
-static unsigned int errorInt = 4008636142;
+static   uint32_t errorInt = 4008636142;
 static int dataLength = 16; // since the p and ad can be arbitrary long, set the data length first, defualt 16 
 
 static unsigned char key[ KEY_BYTES] = {0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19}; // 20 bytes key for ascon 80pq
--- usageExample.py
+++ usageExample.py
@@ -67,48 +67,39 @@ ciphertext = padByteArray(bytearray(res_ciphertext, 'ascii'))
 # ============= 
 # to decrypt
 
-key = bytearray([0]*20) # 20 bytes
-
-plaintext =padByteArray( bytearray([1,2]*13)) #  16 bytes of palintext
-plaintextLengthBytes = bytearray([0,0,0,26])  # 16 byte
-plaintextLength = 26 # bytes
-repetitionNum = math.ceil(plaintextLength/16)
-
-associatedData =  padByteArray(bytearray([1,2,3]*12))
-associatedDataLengthBytes =  bytearray([0,0,0,36]) # four bytes
-associatedDataLength = 36
-ciphertextLength = plaintextLength+16 # plus tag
-ciphertextLengthBytes =  bytearray([0,0,0,42])
-
-
-nonce = bytearray([7]*16)#bytes 16
-#
-
+print("==============decrypt===============")
 target.simpleserial_write('b', associatedDataLengthBytes)
-print(target.simpleserial_read('r',  4))
+#print(target.simpleserial_read('r',  4))
+print(target.read());
 for t in range(math.ceil(associatedDataLength/16)):
     target.simpleserial_write('a',associatedData[t*16 : (t+1)*16])
-    print(target.simpleserial_read('r',  16))
+    # print(target.simpleserial_read('r',  16))
+    print(target.read())
 
 target.simpleserial_write('n', nonce)
-print(target.simpleserial_read('r',   16))
+#print(target.simpleserial_read('r',   16))
+print(target.read())
 target.simpleserial_write('k', key)
-print(target.simpleserial_read('r',     20))
+#print(target.simpleserial_read('r',     20))
+
+print(target.read())
 
 target.simpleserial_write('l',ciphertextLengthBytes)
-print(target.simpleserial_read('r',  4))
+#print(target.simpleserial_read('r',  4))
+print(target.read())
 for i in range(repetitionNum):
 
     target.simpleserial_write('c', ciphertext [i*16: (i+1)*16])
-    print(target.simpleserial_read('r',  16))
+    # print(target.simpleserial_read('r',  16))
+    print(target.read())
 
 res_plaintext = ''
 for i in range(repetitionNum):
     target.simpleserial_write('d', bytearray())
-    temp = str(target.simpleserial_read('r',16))
-    print(temp)
+    #temp = str(target.simpleserial_read('r',16))
+    print(target.read())
     
-    res_plaintext+= temp
+    #res_plaintext+= temp
 
 
 
